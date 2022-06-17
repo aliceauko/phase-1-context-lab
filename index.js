3ff2b2352dac24abcbaff9402b68d17cffb34c6b
aliceauko <alice.auko@student.moringaschool.com>
2022-06-17 20:27:29 +0300

    commit tree 4b26186034c0f8d8db5c909fdef553d6349c42d1

--- index.js
+++ index.js
@@ -9,6 +9,62 @@
  for you to use if you need it!
  */
 
+
+function createEmployeeRecord(array){
+    return new Object({
+        firstName : array[0],
+        familyName : array[1],
+        title : array[2],
+        payPerHour : array[3],
+        timeInEvents :[],
+        timeOutEvents :[]
+})
+
+}
+
+let createEmployeeRecords = function(newArray){
+    return newArray.map(function(array){
+        return createEmployeeRecord(array)
+    })
+}
+let createTimeInEvent= function(dates){
+ let [date,hour] =dates.split('')
+
+ this.timeInEvents.push({
+    type: "TimeIn",
+    date,
+    hour :parseInt(hour,10),
+ })
+ return this
+}
+
+let createTimeOutEvent= function(datesOut){
+    let [date,hour] =datesOut.split('')
+   
+    this.timeOutEvents.push({
+       type: "TimeOut",
+       date,
+       hour :parseInt(hour,10),
+    })
+    return this
+   }
+
+
+
+
+function hoursWorkedOnDate (recordObject, param1){
+    let hours = (recordObject.timeOutEvents[0].hour -recordObject.timeInEvents[0].hour)/100
+    return hours
+}
+
+
+
+function wagesEarnedOnDate(recordObject,param1){
+    let wages = hoursWorkedOnDate(recordObject, param1)*recordObject.payPerHour
+    return wages
+
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +77,16 @@ const allWagesFor = function () {
     return payable
 }
 
+
+
+
+
+function calculatePayroll(employees){
+ let total = 0
+ for (let employee of employees){
+    console.log(employee)
+    total += allWagesFor(employee)
+ }
+ return total 
+}
+console.log (calculatePayroll(employees))
